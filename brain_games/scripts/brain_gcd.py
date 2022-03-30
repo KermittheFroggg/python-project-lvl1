bd90065593d590c574adf02893744c5abaed236c
KermittheFroggg <santamantana@gmail.com>
2022-03-30 20:38:00 +0300

    2

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from brain_games.games.main_file import start_game
+from brain_games.games.main_file import main_game
 
 
 def main():
     print('Welcome to the Brain Games!')
-    start_game('gcd')
+    main_game('gcd')
 
 
 if __name__ == '__main__':
